054e2338f074842876d55d87f38b8f8fca8d29b1
GrandMaster2004 <121103159+GrandMaster2004@users.noreply.github.com>
2024-04-09 18:09:09 +0530

    by yash

---
+++ src/components/pages/Yash/Our_goal.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { IoBulbOutline } from "react-icons/io5";
+const Our_goal = () => {
+  return (
+    <div className="card_content">
+      <div className="icon_right">
+        <IoBulbOutline className="icon_icon" />
+      </div>
+      <div className="content">
+        <h4 className="title">Mission</h4>
+        <p className="content_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+          autem illo quasi fugit commodi eaque iusto deleniti illum, vel, sit
+          aperiam soluta, voluptatem recusandae. Nisi beatae autem dolorum
+          placeat
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Our_goal;
--- src/components/pages/home/Yash/components/Opportunities.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Opportunities = () => {
-  return <div></div>;
-};
-
-export default Opportunities;
